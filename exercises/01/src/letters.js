0b1dd5bddcea87c17b83f25e5c60477d4aace934
ola <72313610+olkanalazek@users.noreply.github.com>
2021-07-16 14:36:42 +0200

    update

--- exercises/01/src/letters.js
+++ exercises/01/src/letters.js
@@ -1,5 +1,15 @@
-const count = () => {
-  return 1;
+const count = (input) => {
+  let result = {};
+
+  for (let x = 2; x < 10; x++) {
+    const char = input[x];
+
+    if (char) {
+      result[char] = 1;
+    }
+  }
+
+  return result;
 };
 
 export { count };
--- exercises/01/tests/letters.test.js
+++ exercises/01/tests/letters.test.js
@@ -1,7 +1,14 @@
 import { count } from "../src/index";
 
 describe("letters", () => {
-  it("should return empty result for empty string", () => {
-    expect(count("blah")).toEqual({});
+  test.each(["", null, undefined])(
+    "should return empty result for empty string - .count(%s)",
+    (input) => {
+      expect(count("")).toEqual({});
+    }
+  );
+
+  test("sanity check", () => {
+    expect(count("olaolaolaola")).toEqual({ o: 1, l: 1, a: 1 });
   });
 });
