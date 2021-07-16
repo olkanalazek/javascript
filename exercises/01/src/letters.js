53b6ea0240295de676fa4633af7b2f57b6f1bfa2
ola <72313610+olkanalazek@users.noreply.github.com>
2021-07-16 14:50:42 +0200

    update

--- exercises/01/src/letters.js
+++ exercises/01/src/letters.js
@@ -1,15 +1,13 @@
 // count("ola") => { o: 1, l: 1, a: 1 }
-// count("olaola") => { o: 2, l: 2, a: 2 }
+// count("olaola") => { o: 2, l: 2, a: 2x }
 
 const count = (input) => {
   let result = {};
 
-  for (let x = 2; x < 10; x++) {
+  for (let x = 0; x < input.length; x++) {
     const char = input[x];
 
-    if (char) {
-      result[char] = 1;
-    }
+    result[char] = (result[char] || 0) + 1;
   }
 
   return result;
--- exercises/01/tests/letters.test.js
+++ exercises/01/tests/letters.test.js
@@ -8,11 +8,11 @@ describe("letters", () => {
     }
   );
 
-  test("sanity check 1", () => {
+  test("should count characters (olaolaola)", () => {
     expect(count("olaolaola")).toEqual({ o: 3, l: 3, a: 3 });
   });
 
-  test("sanity check 2", () => {
+  test("should count characters (olaolaolaola!)", () => {
     expect(count("olaolaolaola!")).toEqual({ o: 4, l: 4, a: 4, "!": 1 });
   });
 });
