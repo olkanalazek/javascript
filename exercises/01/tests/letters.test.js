b20a28beb3d15c2da475a5912fcbc98a688819eb
ola <72313610+olkanalazek@users.noreply.github.com>
2021-07-16 14:25:20 +0200

    update

--- exercises/01/tests/letters.test.js
+++ exercises/01/tests/letters.test.js
@@ -1,11 +1,7 @@
-import { count } from "../src/letters";
+import { count } from "../src/index";
 
-test("two plus two is four", () => {
-  expect(2 + 2).toBe(4);
-});
-
-describe("xyz", () => {
-  it("aaa", () => {
-    expect(10).toEqual(10);
+describe("letters", () => {
+  it("should return empty result for empty string", () => {
+    expect(count("blah")).toEqual({});
   });
 });
