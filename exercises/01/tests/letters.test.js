3d20fe2709e157c5570d855519efed85eefbb1e4
ola <72313610+olkanalazek@users.noreply.github.com>
2021-07-16 14:01:15 +0200

    update

---
+++ exercises/01/tests/letters.test.js
@@ -0,0 +1,11 @@
+import { count } from "../src/letters";
+
+test("two plus two is four", () => {
+  expect(2 + 2).toBe(4);
+});
+
+describe("xyz", () => {
+  it("aaa", () => {
+    expect(10).toEqual(10);
+  });
+});
